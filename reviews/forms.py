7d0b5a10fb58acbb452215a835e71f26f5888d20
pangpyo <kkp0639@gmail.com>
2022-10-07 16:28:10 +0900

    KP final

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,8 +1,17 @@
-from django import forms
+from django.forms import ModelForm, NumberInput
 from .models import Reviews
 
 
-class ReviewForm(forms.ModelForm):
+class ReviewForm(ModelForm):
     class Meta:
         model = Reviews
         fields = "__all__"
+        widgets = {
+            "grade": NumberInput(
+                attrs={
+                    "maxlength": "1",
+                    "max": "5",
+                    "min": "1",
+                }
+            ),
+        }
